fec7e81d0aa129e7f5e3e60f86e431fed3a1d2f1
Hishen Sandaru Perera <hishenperera@gmail.com>
2025-04-03 15:38:33 +0530

    add different color

--- hello-app/pages/index.vue
+++ hello-app/pages/index.vue
@@ -2,15 +2,27 @@
 
 <template>
   <Header />
-  <div class="flex flex-col items-center justify-center min-h-screen bg-[#176e43]">
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-[#176e43]"
+  >
     <h1 class="text-4xl font-bold text-[#bfffde]">Hello Hishen</h1>
-    <h1 class="text-4xl font-bold text-[#bfffde] drop-shadow-md">Welcome to Nuxt Js</h1>
+    <div class="flex">
+      <h1
+        class="text-4xl font-bold text-[#bfffde] drop-shadow-md"
+        style="margin-right: 10px"
+      >
+        Welcome to
+      </h1>
+      <h1 class="text-4xl font-bold text-[#18ff88] drop-shadow-md">
+        NUXT Framework
+      </h1>
+    </div>
   </div>
   <Footer />
 </template>
 
 <style scoped>
-h1{
-    font-family: 'Poppins';
+h1 {
+  font-family: "Poppins";
 }
 </style>
