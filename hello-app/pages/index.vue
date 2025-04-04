ae8e9df7126e0b1be0873f8fe710b2bd83063ebf
HishenPerera <hishenperera@gmail.com>
2025-04-04 09:45:57 +0530

    fix index js

--- hello-app/pages/index.vue
+++ hello-app/pages/index.vue
@@ -5,15 +5,7 @@ definePageMeta({
 </script>
 
 <template>
-<<<<<<< HEAD
-  
   <div class="flex flex-col items-center justify-center min-h-screen bg-[#176e43]">
-=======
-  <Header />
-  <div
-    class="flex flex-col items-center justify-center min-h-screen bg-[#176e43]"
-  >
->>>>>>> e78188088ab18dbfcc4d5889bf9196599f5c9c96
     <h1 class="text-4xl font-bold text-[#bfffde]">Hello Hishen</h1>
     <div class="flex">
       <h1
